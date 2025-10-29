0c414d74db8e0718ec046692fbc0998287fe008d
santi1928 <linah@colexp.com>
2025-10-29 09:30:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ let contentswiper = new Swiper(".slider-wrapper", {
 let heroSwiper = new Swiper(".hero-swiper", {
   loop: true,
   autoplay: {
-    delay: 10000,
+    delay: 6000,
     disableOnInteraction: false,
   },
   pagination: {
@@ -146,4 +146,5 @@ document.addEventListener('keydown', function(event) {
     });
     document.body.style.overflow = 'auto';
   }
+
 });
